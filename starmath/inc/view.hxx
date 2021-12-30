f462cd8d95335fffec3bfd14e61231a0df9c580c
Jan Rheinländer <jrheinlaender@users.sourceforge.net>
2021-12-30 19:59:55 +0100

    iMath: Change Math formula edit window to tabbed view for Starmath and iMath

--- starmath/inc/view.hxx
+++ starmath/inc/view.hxx
@@ -175,10 +175,10 @@ public:
 
 class SmEditController final : public SfxControllerItem
 {
-    SmEditWindow &rEdit;
+    AbstractEditWindow &rEdit;
 
 public:
-    SmEditController(SmEditWindow &, sal_uInt16, SfxBindings  & );
+    SmEditController(AbstractEditWindow &, sal_uInt16, SfxBindings  & );
 
     virtual void StateChangedAtToolBoxControl(sal_uInt16 nSID, SfxItemState eState, const SfxPoolItem* pState) override;
 };
@@ -186,7 +186,9 @@ public:
 class SmCmdBoxWindow final : public SfxDockingWindow
 {
     std::unique_ptr<SmEditWindow, o3tl::default_delete<SmEditWindow>> m_xEdit;
+    std::unique_ptr<ImEditWindow, o3tl::default_delete<ImEditWindow>> m_xImEdit;
     SmEditController    aController;
+    SmEditController    aImController;
     bool                bExiting;
 
     Timer               aInitialFocusTimer;
@@ -218,10 +220,8 @@ public:
 
     void AdjustPosition();
 
-    SmEditWindow& GetEditWindow()
-    {
-        return *m_xEdit;
-    }
+    AbstractEditWindow& GetEditWindow();
+
     SmViewShell* GetView();
 };
 
@@ -233,7 +233,7 @@ class SmCmdBoxWrapper final : public SfxChildWindow
 
 public:
 
-    SmEditWindow& GetEditWindow()
+    AbstractEditWindow& GetEditWindow()
     {
         return static_cast<SmCmdBoxWindow *>(GetWindow())->GetEditWindow();
     }
@@ -299,7 +299,7 @@ public:
         return static_cast<SmDocShell *>( GetViewFrame().GetObjectShell() );
     }
 
-    SmEditWindow * GetEditWindow();
+    AbstractEditWindow * GetEditWindow();
 
     SmGraphicWidget& GetGraphicWidget()
     {
