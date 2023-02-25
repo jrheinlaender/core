fa40e2ffb5d5cc3a41eb64e927e493001601d1e5
Jan Rheinländer <jrheinlaender@users.sourceforge.net>
2023-02-25 17:32:53 +0100

    iMath: Implement formula hide/show
    Change-Id: Ib75951d0ebed4df6c39ac1c4bead7d3d038db671

--- sw/source/uibase/shells/frmsh.cxx
+++ sw/source/uibase/shells/frmsh.cxx
@@ -793,6 +793,22 @@ void SwFrameShell::Execute(SfxRequest &rReq)
                 }
             }
         break;
+        case FN_IMATH_EDIT_HIDE:
+            {
+            svt::EmbeddedObjectRef& xObj = rSh.GetOLEObject();
+
+                if(xObj.is())
+                    rSh.GetDoc()->GetDocShell()->HideIFormula(rSh.GetFlyName(), true);
+            }
+        break;
+        case FN_IMATH_EDIT_SHOW:
+            {
+            svt::EmbeddedObjectRef& xObj = rSh.GetOLEObject();
+
+                if(xObj.is())
+                    rSh.GetDoc()->GetDocShell()->HideIFormula(rSh.GetFlyName(), false);
+            }
+        break;
         default:
             assert(!"wrong dispatcher");
             return;
@@ -1115,6 +1131,8 @@ void SwFrameShell::GetState(SfxItemSet& rSet)
             break;
             case FN_IMATH_INSERT_CONVERT:
             case FN_IMATH_EDIT_MERGE:
+            case FN_IMATH_EDIT_HIDE:
+            case FN_IMATH_EDIT_SHOW:
             {
                 SvtModuleOptions aMOpt;
                 const SelectionType nType = rSh.GetSelectionType();
@@ -1141,6 +1159,8 @@ void SwFrameShell::GetState(SfxItemSet& rSet)
                                         rSet.DisableItem(nWhich);
                                 break;
                                 case FN_IMATH_EDIT_MERGE:
+                                case FN_IMATH_EDIT_HIDE:
+                                case FN_IMATH_EDIT_SHOW:
                                     // Requires a math formula to be selected that is an iFormula
                                     if (isMathOnly)
                                         rSet.DisableItem(nWhich);
